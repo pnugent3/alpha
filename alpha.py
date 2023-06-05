70fc1edca805e0e55eea66c1718fd97e6e91d29e
Pete Nugent <pnugent3@gmail.com>
2023-06-05 10:38:04 +0200

    first. Only prints help text.

---
+++ alpha.py
@@ -0,0 +1,20 @@
+""" How quick can you type the alphabet?
+"""
+
+import time
+
+ipVal = 0
+bestTime = 0
+
+def main():
+	print("Time how quickly you can type the alphabet A->Z")
+	print("(Either lower case or upper, it doesn't matter)")
+	print("The time will begin when you press A and end when you press Z")
+	print("Press ESC to abort")
+	
+	
+
+
+
+if __name__ == "__main__":
+    main()
